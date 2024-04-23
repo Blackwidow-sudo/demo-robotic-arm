f4d14cc1a8c2757694bf70e705aef18233f3d57a
Evgenij Myasnikov <emyasnikov@mkw.eu>
2024-04-23 15:09:20 +0200

    Added main script.

---
+++ main.py
@@ -0,0 +1,29 @@
+import gradio as gr
+import torch
+from PIL import Image
+from ultralytics import YOLO
+
+MODEL_NAME = 'drogerie_8_yolov8s-seg_8k_v1.pt'
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def predict(image):
+    model = YOLO(MODEL_NAME).to(device)
+    results = model.predict(image)
+
+    for r in results:
+        image_array = r.plot(boxes=True)
+        pil_image = Image.fromarray(image_array[..., ::-1])
+
+    return pil_image
+
+app = gr.Interface(
+    allow_flagging=False,
+    css='footer {visibility: hidden}',
+    fn=predict,
+    inputs=gr.Image(type='pil', label='Input Image'),
+    outputs=gr.Image(type='pil', label='Output Image'),
+)
+
+if __name__ == '__main__':
+    app.queue().launch(root_path='/video')
