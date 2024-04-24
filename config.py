b277c5190c8dc614b27659f741135545aeda781c
Evgenij Myasnikov <emyasnikov@mkw.eu>
2024-04-24 16:48:31 +0200

    Added config get as function.

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ from dotenv import dotenv_values
 
 values = dotenv_values(".env")
 
-def get(key, type=str):
+def get(key):
+    return values.get(key, None)
+
+def get_as(key, type=str):
     value = values.get(key, None)
 
     return value if value is None else type(value)
