88c2816852b8639d9c140bf85553cd6674647846
Merlin Cornehl <mcornehl@mkw.eu>
2024-04-24 14:46:40 +0200

    feat: add mps support

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 import config
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
 def clear():
     input_image.value = None
