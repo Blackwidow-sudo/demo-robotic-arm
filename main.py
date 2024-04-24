eaa1ca35f2dc2bb86bb2f5fa6d596a1443223713
Evgenij Myasnikov <emyasnikov@mkw.eu>
2024-04-24 15:02:19 +0200

    Changed env example.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import config
 import gradio as gr
 import torch
 from PIL import Image
 from ultralytics import YOLO
-
-import config
 
 device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
