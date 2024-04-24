a5f6ae2dbbe4e05ddb5babbea5876b0490aa1a57
Evgenij Myasnikov <emyasnikov@mkw.eu>
2024-04-24 16:19:41 +0200

    Changed config get function to accept type conversion.

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ from dotenv import dotenv_values
 
 values = dotenv_values(".env")
 
-def get(key):
-    return values.get(key, None)
+def get(key, type=str):
+    value = values.get(key, None)
+
+    return value if value is None else type(value)
