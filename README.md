74f6f9a57d9fb442410c9977e42c7a74595c5d5b
Merlin Cornehl <mcornehl@mkw.eu>
2024-04-25 10:16:06 +0200

    refactor: image scewing & styles

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Start gradio dev server with:
 ```
 gradio main.py
 ```
+
+Update requirements.txt with:
+```
+pip3 freeze > requirements.txt
+```
--- main.py
+++ main.py
@@ -8,9 +8,39 @@ from ultralytics import YOLO
 
 device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
+custom_css = """
+    footer {
+        visibility: hidden;
+    }
+
+    .app.gradio-container {
+        max-width: 100% !important;
+    }
+"""
+
+
+"""Skew image so that the table box is parallel to the image edges"""
+def unscew_img(image: Image, top_left, top_right, bottom_left, bottom_right) -> Image:
+    np_img = np.array(image)
+    height, width = np_img.shape[:2]
+
+    src_pts = np.float32([list(top_left), list(top_right), list(bottom_left), list(bottom_right)])
+    dest_pts = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+    matrix = cv.getPerspectiveTransform(src_pts, dest_pts)
+    warped_img = cv.warpPerspective(np_img, matrix, (width, height))
+
+    return Image.fromarray(warped_img)
+    
+
 def predict(image, left_top_x, left_top_y, right_top_x, right_top_y, left_bottom_x, left_bottom_y, right_bottom_x, right_bottom_y, width, height):
     model = YOLO(config.get('MODEL_NAME')).to(device)
     results = model.predict(image)
+
+    left_top = (left_top_x, left_top_y)
+    right_top = (right_top_x, right_top_y)
+    left_bottom = (left_bottom_x, left_bottom_y)
+    right_bottom = (right_bottom_x, right_bottom_y)
 
     for r in results:
         image_array = r.plot(boxes=True)
@@ -19,22 +49,19 @@ def predict(image, left_top_x, left_top_y, right_top_x, right_top_y, left_bottom
     if config.get_bool('DRAW_CALIBRATION'):
         draw = ImageDraw.Draw(pil_image)
 
-        draw.line([(left_top_x, left_top_y), (right_top_x, right_top_y)], fill='red', width=2)
-        draw.line([(right_top_x, right_top_y), (right_bottom_x, right_bottom_y)], fill='red', width=2)
-        draw.line([(right_bottom_x, right_bottom_y), (left_bottom_x, left_bottom_y)], fill='red', width=2)
-        draw.line([(left_bottom_x, left_bottom_y), (left_top_x, left_top_y)], fill='red', width=2)
+        draw.line([left_top, right_top], fill='red', width=2)
+        draw.line([right_top, right_bottom], fill='red', width=2)
+        draw.line([right_bottom, left_bottom], fill='red', width=2)
+        draw.line([left_bottom, left_top], fill='red', width=2)
 
-    source_points = np.array([[left_top_x, left_top_y], [right_top_x, right_top_y], [right_bottom_x, right_bottom_y], [left_bottom_x, left_bottom_y]])
-    destination_points = np.array([[0, 0], [width, 0], [width, height], [0, height]])
-
-    matrix = cv.getPerspectiveTransform(source_points.astype(np.float32), destination_points.astype(np.float32))
-    warped_image = cv.warpPerspective(np.array(pil_image), matrix, (width, height))
+    warped_image = unscew_img(pil_image, left_top, right_top, left_bottom, right_bottom)
 
     return warped_image if config.get_bool('OUTPUT_WARPED') else pil_image, [left_top_x, left_top_y, right_top_x, right_top_y, left_bottom_x, left_bottom_y, right_bottom_x, right_bottom_y, width, height]
 
-with gr.Blocks(css='footer {visibility: hidden}') as demo:
+
+with gr.Blocks(css=custom_css) as demo:
     with gr.Row():
-        input_image = gr.Image(type='pil', label='Input Image', sources=['webcam'])
+        input_image = gr.Image(type='pil', label='Input Image', sources=['webcam'], streaming=True)
         output_image = gr.Image(type='pil', label='Output Image')
 
     with gr.Accordion('Calibration', open=False):
@@ -67,6 +94,7 @@ with gr.Blocks(css='footer {visibility: hidden}') as demo:
         button_submit = gr.Button(value='Submit', variant='primary')
 
     button_submit.click(fn=predict, inputs=[input_image, left_top_x, left_top_y, right_top_x, right_top_y, left_bottom_x, left_bottom_y, right_bottom_x, right_bottom_y, width, height], outputs=[output_image, output_text])
+
 
 if __name__ == '__main__':
     demo.queue().launch(root_path='/video')
